26db231ceaf0abbb6a23576a8a16e2bff4765170
aliya <2995293916@qq.com>
2018-08-26 00:36:08 +0800

    add icons

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,11 +1,18 @@
 <template>
 	<div class="icons">
-		<div class="icon" v-for="item of iconList" :key="item.id">
-			<div class="icon-img">
-				<img :src="item.imgUrl" class="icon-imgcontent"/>
-			</div>
-			<p class="icon-desc">TEXT</p>
-		</div>
+		<swiper>
+			<swiper-slide v-for="(page,index) of pages" :key="index">
+				<div class="icon" 
+					v-for="(item,index) of page" 
+					:key='item.id'>
+					<div class="icon-img">
+						<img :src="item.imgUrl" class="icon-img-content"/>
+					</div>
+					<p class="icon-desc">{{item.desc}}</p>
+				</div>
+			</swiper-slide>
+			
+		</swiper>
 	</div>
 </template>
 
@@ -16,30 +23,55 @@
 			return{
 				iconList:[{
 					id:"001",
-					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+					desc:'景点门票'
 				},{
 					id:"002",
-					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+					desc:'景点门票'
 				},{
 					id:"003",
-					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+					desc:'景点门票'
 				},{
 					id:"004",
-					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+					desc:'景点门票'
 				},{
 					id:"005",
-					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+					desc:'景点门票'
 				},{
 					id:"006",
-					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+					desc:'景点门票'
 				},{
 					id:"007",
-					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+					desc:'景点门票'
 				},{
 					id:"008",
-					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
-				},
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+					desc:'景点门票'
+				},{
+					id:"009",
+					imgUrl:"http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+					desc:'景点门票'
+				}
 				]
+			}
+		},
+		computed:{
+			pages () {
+				const pages = []
+				this.iconList.forEach((item,index) =>{
+					const page =Math.floor(index/8)
+					if(!pages[page]){
+						pages[page] =[]
+					}
+					pages[page].push(item)
+				})
+				return pages
 			}
 		}
 	}
@@ -47,38 +79,37 @@
 
 <style lang="stylus" scoped>
 	@import '~styles/variables.styl'
-	.icons
-		overflow:hidden
+	@import '~styles/mixins.styl'
+	.icons >>> .swiper-container
 		height:0
 		padding-bottom:50%
-		.icon
-			position:relative
-			overflow:hidden
-			float:left
-			width:25%
-			padding-bottom:25%
-			height:0
-			.icon-img
-				position:absolute
-				top:0
-				left:0
-				right:0
-				bottom:.44rem
-				box-sizing:border-box
-				padding.1rem
-				.icon-imgcontent
-					display:block
-					margin:auto
-					height:100%
-			.icon-desc{
-				position:absolute
-				left:0
-				right:0
-				bottom:0
-				line-height:.44rem
-				height:.44rem
-				color:$darkTextColor
-				text-align:center
-			}
-					
+	.icon
+		position:relative
+		overflow:hidden
+		float:left
+		width:25%
+		padding-bottom:25%
+		height:0
+		.icon-img
+			position:absolute
+			top:0
+			left:0
+			right:0
+			bottom:.44rem
+			box-sizing:border-box
+			padding.1rem
+			.icon-img-content
+				display:block
+				margin:0 auto
+				height:100%
+		.icon-desc
+			position:absolute
+			left:0
+			right:0
+			bottom:0
+			line-height:.44rem
+			height:.44rem
+			color:$darkTextColor
+			text-align:center
+			ellipsis()
 </style>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,7 +2,7 @@
 <div class="wrapper">
  <swiper :options="swiperOption">
     <swiper-slide v-for="item of swiperList" :key="item.id">
-    	<img class="swiper-image" :src="item.imgUrl" />
+    		<img class="swiper-image" :src="item.imgUrl" />
     </swiper-slide>
     <div class="swiper-pagination"  slot="pagination"></div>
   </swiper>
