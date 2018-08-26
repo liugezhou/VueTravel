e7b718cd3418da2bc2653a39ee0c0bae63d3d176
aliya <2995293916@qq.com>
2018-08-26 22:25:54 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,74 @@
 <template>
-	<div class="search">
-		<input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+	<div>
+		<div class="search">
+			<input class="search-input" 
+				type="text" 
+				v-model="keyword"
+				placeholder="输入城市名或拼音"/>
+		</div>
+		<div class="search-content" 
+			ref="search"
+			v-show="keyword"
+			>
+			<ul>
+				<li class="search-item border-bottom" 
+					v-for ="item of list"
+					:key="item.id">
+					{{item.name}}
+				</li>
+				<li class="search-item border-bottom" v-show='hasNoData'>
+					没有找到匹配数据
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
 <script>
+	import Bscroll from 'better-scroll'
 	export default {
-		name:'CitySearch'
+		name:'CitySearch',
+		props:{
+			cities:Object	
+		},
+		data () {
+			return {
+				keyword:'',
+				list:[],
+				timer:null
+			}
+		},
+		computed :{
+			hasNoData () {
+				return  !this.list.length
+			}
+		},
+		watch : {
+			keyword () {
+				if(this.timer){
+					clearTimeout(this.timer)
+				}
+				if(!this.keyword){
+					this.list =[]
+					return
+				}
+				this.timer = setTimeout(()=>{
+					const result =[]
+					for (let i in this.cities){
+						this.cities[i].forEach((value) => {
+							if(value.spell.indexOf(this.keyword) >-1||
+							value.name.indexOf(this.keyword)>-1){
+								result.push(value)
+							}
+						})
+					}
+					this.list =result
+				} ,100)
+			}
+		},
+		mounted () {
+			this.scroll =new Bscroll(this.$refs.search)
+		}
 	}
 </script>
 
@@ -25,4 +87,18 @@
 			text-align:center
 			border-radius:.06rem
 			color:#666
+	.search-content
+		overflow:hidden
+		z-index:1
+		position:absolute
+		top:1.58rem
+		left:0
+		right:0
+		bottom:0
+		background:#eee
+		.search-item
+			line-height:.62rem
+			padding-left:.2rem
+			color:#666
+			background:#fff
 </style>
