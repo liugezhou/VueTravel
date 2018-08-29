4795fd08f81c0b90cb9ce4790ba2eeaecdf8f34c
aliya <2995293916@qq.com>
2018-08-29 20:11:08 +0800

    detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,6 +2,7 @@
 	<div>
 		<detail-banner></detail-banner>
 		<detail-header></detail-header>
+		<detail-list :list="list"></detail-list>
 		<div class="content"></div>
 	</div>
 </template>
@@ -10,16 +11,45 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
 	name:'Detail',
 	components:{
 		DetailBanner,
-		DetailHeader
+		DetailHeader,
+		DetailList
+	},
+	data () {
+		return {
+			list:[{
+				title:'成人票',
+				children:[{
+					title:"成人三馆联票",
+					children:[{
+					title:"成人三馆联票-第三季"
+				},{
+					title:"成人三馆联票-第三季"
+				},{
+					title:"成人三馆联票-第三季"
+				}]
+				},{
+					title:"成人三馆联票"
+				},{
+					title:"成人三馆联票"
+				}]
+			},{
+				title:'学生票'
+			},{
+				title:'儿童票'
+			},{
+				title:'特惠票'
+			}]
+		}
 	}
 }
 </script>
 
 <style lang="stylus" scoped>
 	.content
-		height:50rem;
+		height:15rem;
 </style>
--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -72,6 +72,7 @@
 			color:#fff
 			font-size:.4rem
 	.header-fixed
+		z-index:1
 		position:fixed
 		top:0
 		left:0
